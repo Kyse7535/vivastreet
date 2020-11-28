c2dd1aae1e4bf3fa9bd4b93a888d66efe3b50cb1
Kyse7535 <ktevot@gmail.com>
2020-11-28 10:00:30 +0100

    vivastreet clone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { side_bar } from '../../side_bar/side_bar';
 
+
 $(document).ready(function() {
+
     side_bar.display_side_bar();
 
     $('.immobilier').click(() => {
@@ -27,4 +29,31 @@ $(document).ready(function() {
     $('.rencontres').click(() => {
         $('#rencontres').slideToggle();
     });
+
+    $('.categorie_immobilier,.list_items_immobilier').hover(function() {
+        $('.list_items_immobilier').toggleClass('deroulant');
+    });
+    $('.categorie_emploi, .list_items_emploi').hover(function() {
+        $('.list_items_emploi').toggleClass('deroulant');
+    });
+    $('.categorie_vehicule, .list_items_vehicule').hover(function() {
+        $('.list_items_vehicule').toggleClass('deroulant');
+    });
+    $('.categorie_services, .list_items_services').hover(function() {
+        $('.list_items_services').toggleClass('deroulant');
+    });
+    $('.categorie_cours, .list_items_cours').hover(function() {
+        $('.list_items_cours').toggleClass('deroulant');
+    });
+    $('.categorie_animaux, .list_items_animaux').hover(function() {
+        $('.list_items_animaux').toggleClass('deroulant');
+    });
+    $('.categorie_plans, .list_items_plans').hover(function() {
+        $('.list_items_plans').toggleClass('deroulant');
+    });
+    $('.categorie_rencontres, .list_items_rencontres').hover(function() {
+        $('.list_items_rencontres').toggleClass('deroulant');
+    });
+
+
 });
